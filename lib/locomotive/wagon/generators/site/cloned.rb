ed147629b3e9b836cd33c6f16f13a4b1eeb35031
did <didier.lafforgue@gmail.com>
2013-04-12 18:10:22 +0200

    clone command + fix various bugs

---
+++ lib/locomotive/wagon/generators/site/cloned.rb
@@ -0,0 +1,23 @@
+module Locomotive
+  module Wagon
+    module Generators
+      module Site
+
+        # Template used when the remote LocomotiveCMS site is cloned
+        # in order to have the minimal files.
+        class Cloned < Base
+
+          argument :connection_info
+
+          def copy_sources
+            directory('.', self.destination, { recursive: true }, {
+              name:     self.name,
+              version:  Locomotive::Wagon::VERSION
+            }.merge(self.connection_info))
+          end
+
+        end
+      end
+    end
+  end
+end
