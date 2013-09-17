2267e8a29bf8f4b32d461f721f3efeae3a850581
did <didier.lafforgue@gmail.com>
2013-09-17 10:25:48 +0200

    the consume tag is consistent with the one from the engine

--- lib/locomotive/wagon/liquid/tags/consume.rb
+++ lib/locomotive/wagon/liquid/tags/consume.rb
@@ -2,7 +2,6 @@ module Locomotive
   module Wagon
     module Liquid
         module Tags
-
         # Consume web services as easy as pie directly in liquid !
         #
         # Usage:
@@ -15,37 +14,72 @@ module Locomotive
         #
         class Consume < ::Liquid::Block
 
-          Syntax = /(#{::Liquid::VariableSignature}+)\s*from\s*(#{::Liquid::QuotedFragment}+)/
+          Syntax = /(#{::Liquid::VariableSignature}+)\s*from\s*(#{::Liquid::QuotedString}|#{::Liquid::VariableSignature}+)/
 
           def initialize(tag_name, markup, tokens, context)
             if markup =~ Syntax
-              @target   = $1
-              @url      = $2.gsub(/['"]/, '')
-              @options  = {}
+              @target = $1
 
-              markup.scan(::Liquid::TagAttributes) do |key, value|
-                @options[key] = value if key != 'http'
-              end
-
-              @options.delete('expires_in')
+              self.prepare_url($2)
+              self.prepare_options(markup)
             else
               raise ::Liquid::SyntaxError.new("Syntax Error in 'consume' - Valid syntax: consume <var> from \"<url>\" [username: value, password: value]")
             end
+
+            @cache_key = Digest::SHA1.hexdigest(@target)
+            @local_cache_key = self.hash
 
             super
           end
 
           def render(context)
+            if @is_var
+              @url = context[@url]
+            end
+            render_all_without_cache(context)
+          end
+
+          protected
+
+          def prepare_url(url)
+            @url    = url
+            @is_var = false
+            if @url.match(::Liquid::QuotedString)
+              @url.gsub!(/['"]/, '')
+            elsif @url.match(::Liquid::VariableSignature)
+              @is_var = true
+            else
+              raise ::Liquid::SyntaxError.new("Syntax Error in 'consume' - Valid syntax: consume <var> from \"<url>\" [username: value, password: value]")
+            end
+          end
+
+          def prepare_options(markup)
+            @options = {}
+            markup.scan(::Liquid::TagAttributes) do |key, value|
+              @options[key] = value if key != 'http'
+            end
+            @options['timeout'] = @options['timeout'].to_f if @options['timeout']
+            @expires_in = (@options.delete('expires_in') || 0).to_i
+          end
+
+          def cached_response
+            @@local_cache ||= {}
+            @@local_cache[@local_cache_key]
+          end
+
+          def cached_response=(response)
+            @@local_cache ||= {}
+            @@local_cache[@local_cache_key] = response
+          end
+
+          def render_all_without_cache(context)
             context.stack do
-              _response = nil
-
               begin
-                _response = Locomotive::Wagon::Httparty::Webservice.consume(@url, @options.symbolize_keys)
-              rescue Exception => e
-                _response = { 'error' => e.message.to_s }.to_liquid
+                context.scopes.last[@target.to_s] = Locomotive::Wagon::Httparty::Webservice.consume(@url, @options.symbolize_keys)
+                self.cached_response = context.scopes.last[@target.to_s]
+              rescue Timeout::Error
+                context.scopes.last[@target.to_s] = self.cached_response
               end
-
-              context.scopes.last[@target.to_s] = _response
 
               render_all(@nodelist, context)
             end
