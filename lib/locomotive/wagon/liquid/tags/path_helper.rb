763374cb515c0c88de20b9856d82dd16a9496c86
Vandendriessche Gil <gil@dandycoding.com>
2013-12-27 10:07:56 +0100

    Bug fix

--- lib/locomotive/wagon/liquid/tags/path_helper.rb
+++ lib/locomotive/wagon/liquid/tags/path_helper.rb
@@ -77,7 +77,7 @@ module Locomotive
               page.fullpath.clone
             end
 
-            fullpath = "#{::I18n.locale}/#{fullpath}" if ::I18n.locale.to_s != mounting_point.default_locale.to_s && !@_options['force_locale']
+            fullpath = "#{::I18n.locale}/#{fullpath}" if ::I18n.locale.to_s != mounting_point.default_locale.to_s || @_options['force_locale']
 
             if page.templatized?
               if page.content_entry._slug.nil?
