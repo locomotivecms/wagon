b18539861909bc93effbbffd9177c054c82a24e3
arnaud sellenet <arnodmental@gmail.com>
2014-04-08 15:45:05 +0200

    include pry

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@ require "locomotive/wagon"
 require "rspec"
 require "launchy"
 require 'coveralls'
-
+require 'pry'
 Coveralls.wear!
 
 Dir["#{File.expand_path('../support', __FILE__)}/*.rb"].each do |file|
