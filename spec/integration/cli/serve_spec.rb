62b5fe559c428a95cd3f1d0431ede6a19f13ec08
Joel AZEMAR <joel.azemar@gmail.com>
2014-04-22 10:17:24 +0200

    Clean output

--- spec/integration/cli/serve_spec.rb
+++ spec/integration/cli/serve_spec.rb
@@ -1,13 +1,9 @@
-
-# encoding: utf-8
-
 require File.dirname(__FILE__) + '/../integration_helper'
 require 'locomotive/wagon/cli'
 
-
 describe Locomotive::Wagon::CLI::Main do
   describe '#serve' do
-    let(:output) { capture(:stdout) { subject.serve path } }
+    let(:output) { capture(:stdout) { subject.serve path }}
 
     context 'when no site in path' do
       let(:path) { 'a/path/with/no/site' }
@@ -16,7 +12,7 @@ describe Locomotive::Wagon::CLI::Main do
       end
       it 'Does not serve' do
         Locomotive::Wagon.should_not_receive(:serve)
-        subject.serve path
+        quietly { subject.serve path } # active_support / core_ext / kernel / reporting.rb
       end
     end
     context 'when site in path' do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ require 'bundler/setup'
 
 require 'launchy'
 require 'coveralls'
+require 'i18n'
 
 require_relative '../lib/locomotive/wagon'
 require_relative 'support'
@@ -15,7 +16,11 @@ RSpec.configure do |config|
   config.filter_run focused: true
   config.run_all_when_everything_filtered = true
 
-  config.before(:all) { remove_logs }
+  config.before(:all) do
+    I18n.enforce_available_locales = false
+    remove_logs
+  end
+
   config.before { reset! }
 
   config.after  { reset! }
