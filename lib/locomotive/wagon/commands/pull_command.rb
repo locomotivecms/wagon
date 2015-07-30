07b03816a86ba60cfb3c009471919e287cae4640
did <didier.lafforgue@gmail.com>
2015-07-30 12:54:48 +0200

    pull command: content_assets done

--- lib/locomotive/wagon/commands/pull_command.rb
+++ lib/locomotive/wagon/commands/pull_command.rb
@@ -14,7 +14,7 @@ module Locomotive::Wagon
   class PullCommand < Struct.new(:env, :path, :options, :shell)
 
     # RESOURCES = %w(site pages content_types content_entries snippets theme_assets translations).freeze
-    RESOURCES = %w(content_entries).freeze
+    RESOURCES = %w(content_assets).freeze
 
     include ApiConcern
     include DeployFileConcern
--- lib/locomotive/wagon/commands/pull_sub_commands/pull_content_assets_command.rb
+++ lib/locomotive/wagon/commands/pull_sub_commands/pull_content_assets_command.rb
@@ -2,8 +2,21 @@ module Locomotive::Wagon
 
   class PullContentAssetsCommand < PullBaseCommand
 
-    def pull
-      puts 'TODO pulling content assets'
+    def _pull
+      api_client.content_assets.all.each do |asset|
+        write_content_asset(asset)
+      end
+    end
+
+    def write_content_asset(asset)
+      binary = get_asset_binary(asset.url)
+      write_to_file(content_asset_filepath(asset), binary)
+    end
+
+    private
+
+    def content_asset_filepath(asset)
+      File.join('public', 'samples', 'all', asset.content_type_text, asset.filename)
     end
 
   end
