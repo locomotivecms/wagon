140e18f03acffe7a159af390c1fadb12574abc64
Ted Sherwood <ted@tjsconsult.com>
2016-05-17 11:08:08 -0500

    Added generator for Foundation 6

---
+++ generators/foundation6/README.md
@@ -0,0 +1,19 @@
+# Foundation 6 Template for LocomotiveCMS#
+
+This is a basic template for a Foundation for sites 6 based locomotiveCMS site.  Hopefully, it should be a good jumping off point.
+
+## Installation ##
+
+```
+git clone https://github.com/dustb0wl/locomotive-foundation6.git
+cd ./locomotive-foundation6
+bundle install
+bundle exec wagon serve
+```
+
+Read More at the LocomotiveCMS Docs:
+[https://locomotive-v3.readme.io/](https://locomotive-v3.readme.io/)
+
+## License ##
+
+[MIT License](https://opensource.org/licenses/MIT)
---
+++ lib/locomotive/wagon/generators/site/foundation6.rb
@@ -0,0 +1,44 @@
+module Locomotive
+  module Wagon
+    module Generators
+      module Site
+
+        class Foundation6 < Base
+
+          may_use_haml
+          may_use_scss
+
+          def choose_haml_over_html
+            if haml?
+              remove_file File.join(self.destination, 'app/views/pages/index.liquid')
+              remove_file File.join(self.destination, 'app/views/pages/404.liquid')
+            else
+              remove_file File.join(self.destination, 'app/views/pages/index.liquid.haml')
+              remove_file File.join(self.destination, 'app/views/pages/404.liquid.haml')
+            end
+          end
+
+          def choose_scss_over_css
+            if scss?
+              remove_file File.join(self.destination, 'public/stylesheets/app.css')
+              remove_file File.join(self.destination, 'public/stylesheets/foundation.min.css')
+            else
+              remove_dir File.join(self.destination, 'public/stylesheets/foundation6')
+              remove_file File.join(self.destination, 'public/stylesheets/app.scss')
+              remove_file File.join(self.destination, 'public/stylesheets/_settings.scss')
+            end
+          end
+
+          def bundle_install
+            super
+          end
+
+        end
+
+        Locomotive::Wagon::Generators::Site.register(:foundation6, Foundation6, %{
+          A site powered by Foundation (v 6.2.1).
+        })
+      end
+    end
+  end
+end
