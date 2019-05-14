700b1e85c45db0c3ebd16e5133d971e68a5119cf
Didier Lafforgue <didier.lafforgue@gmail.com>
2019-05-14 16:30:31 +0200

    enable the vcr preserve_exact_body_bytes option

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -7,6 +7,10 @@ VCR.configure do |c|
   c.hook_into :faraday
   c.ignore_hosts 'codeclimate.com'
   c.configure_rspec_metadata!
+  c.preserve_exact_body_bytes do |http_message|
+    http_message.body.encoding.name == 'ASCII-8BIT' ||
+    !http_message.body.valid_encoding?
+  end
 end
 
 # Without this, VCR is unable to match a POST request with uploaded files
